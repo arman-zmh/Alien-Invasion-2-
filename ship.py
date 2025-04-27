63cf665b572f1ab7c62cba40f1acc444da0bac8c
arman-zmh <arman.zamaniha123@gmail.com>
2025-04-27 19:15:39 +0330

    full screen

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect() 
+        self.setting = ai_game.settings 
 
         #load the image and get its rect
         self.image = pygame.image.load('image/ship.bmp')
@@ -14,6 +15,7 @@ class Ship:
         
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
         self.moving_right = False
         self.moving_left = False
     
@@ -23,7 +25,9 @@ class Ship:
     
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.setting.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.setting.ship_speed
+
+        self.rect.x = self.x
