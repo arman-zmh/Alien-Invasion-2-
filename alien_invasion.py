22f66fd1b0c9e95c3d5337e797393a22a508c2d9
arman-zmh <arman.zamaniha123@gmail.com>
2025-05-03 18:24:14 +0330

    creating aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,7 +26,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
             
@@ -60,8 +60,21 @@ class AlienInvasion:
             self.ship.moving_left = False
     
     def _fire_bullet(self):
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
+            if len(self.bullets) < self.settings.bullet_allowed:
+                new_bullet = Bullet(self)
+                self.bullets.add(new_bullet)
+            
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            #print(len(self.bullets))
+
+
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
