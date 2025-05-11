0bb8ec2f10a331e41f257a67aea4a5762e502bf7
arman-zmh <arman.zamaniha123@gmail.com>
2025-05-11 20:35:36 +0330

    movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,10 +31,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
-            
-            
+
+    
+
 
     def _check_events(self):
         
@@ -80,7 +82,42 @@ class AlienInvasion:
 
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+    
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            current_x = alien_width
+            current_y += 2 * alien_height
+    
+    
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self):
+            """Drop the entire fleet and change the fleet's direction."""
+            for alien in self.aliens.sprites():
+                alien.rect.y += self.settings.fleet_drop_speed
+                self.settings.fleet_direction *= -1
+            
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+        
+
+    def _create_alien(self, x_position, y_position):
+            new_alien = Alien(self)
+            new_alien.x = x_position
+            new_alien.rect.x = x_position
+            new_alien.rect.y = y_position
+            self.aliens.add(new_alien)
+            
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- settings.py
+++ settings.py
@@ -13,4 +13,8 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 10
         self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 5
+        self.bullet_allowed = 15
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
